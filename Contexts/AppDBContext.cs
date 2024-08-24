9fa7a30f861beb6b7036761a2c845d9ef723aea2
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-08-24 10:37:20 -0300

    :bug: fix: Renomeando arquivo

--- Contexts/AppDBContext.cs
+++ Contexts/AppDBContext.cs
@@ -1,6 +1,26 @@
-﻿namespace ReserveiAPI.Contexts
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using ReserveiAPI.Contexts.Builders;
+using ReserveiAPI.Objects.Models.Entities;
+
+namespace ReserveiAPI.Contexts
 {
-    public class DBContext
+    public class AppDBContext : DbContext
     {
+        // Mapeamento Relacional dos Objetos no Banco de Dados 
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        // Conjunto: Usuário
+        public DbSet<UserModel> Users { get; set; }
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Entidades de Usuário:
+            UserBuilder.Build(modelBuilder);
+        }
     }
 }
--- Contexts/Builders/UserBuilder.cs
+++ Contexts/Builders/UserBuilder.cs
@@ -1,6 +1,32 @@
-﻿namespace ReserveiAPI.Contexts.Builders
+﻿using Microsoft.EntityFrameworkCore;
+using ReserveiAPI.Objects.Models.Entities;
+
+namespace ReserveiAPI.Contexts.Builders
 {
     public class UserBuilder
     {
+        public static void Build(ModelBuilder modelBuilder)
+        {
+            // Builder
+            modelBuilder.Entity<UserDTO>().HasKey(u => u.Id);
+            modelBuilder.Entity<UserDTO>().Property(u => u.ImageProfile);
+            modelBuilder.Entity<UserDTO>().Property(u => u.NameUser).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<UserDTO>().Property(u => u.EmailUser).HasMaxLength(200).IsRequired();
+            modelBuilder.Entity<UserDTO>().Property(u => u.PasswordUser).HasMaxLength(256).IsRequired();
+            modelBuilder.Entity<UserDTO>().Property(u => u.PhoneUser).HasMaxLength(15).IsRequired();
+
+            // Inserções 
+            modelBuilder.Entity<UserDTO>().HasData(
+                new UserDTO
+                {
+                    Id = 1,
+                    NameUser = "master@development.com",
+                    PasswordUser = "2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394",
+                    PhoneUser = "",
+                    ImageProfile = ""
+                }
+            );
+        }
+            
     }
 }
