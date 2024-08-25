42eb0e5ee5d67d323bda89cbb1b7792b87c492b2
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-08-25 20:08:49 -0300

    :sparkles: feat: Criando Server, Startup e modificando a Program.

---
+++ Services/Server/DependenciesInjection.cs
@@ -0,0 +1,15 @@
+﻿using ReserveiAPI.Repositories.Interfaces;
+using ReserveiAPI.Services.Interfaces;
+
+namespace ReserveiAPI.Services.Server
+{
+    public static class DependenciesInjection
+    {
+        public static void AddUserDependencies(this IServiceCollection services)
+        {
+            // Dependência: Usuário
+            services.AddScoped<IUserRepository, IUserRepository>();
+            services.AddScoped<IUserService, IUserService>();
+        }
+    }
+}
